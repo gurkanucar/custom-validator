195bba4dfea0b7ad1a6556dbac4d0a1ea40efc68
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-15 20:34:22 +0300

    feat: project initialized

---
+++ src/main/java/com/gucardev/customvalidator/CustomValidatorApplication.java
@@ -0,0 +1,10 @@
+package com.gucardev.customvalidator;
+
+import org.springframework.boot.SpringApplication;
+
+public class CustomValidatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomValidatorApplication.class, args);
+  }
+}
