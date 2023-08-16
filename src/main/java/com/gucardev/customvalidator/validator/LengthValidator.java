b68750235a7efdd0562f19e3e48180049dec1768
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-16 23:40:55 +0300

    feat: LengthValidator created

---
+++ src/main/java/com/gucardev/customvalidator/validator/LengthValidator.java
@@ -0,0 +1,17 @@
+package com.gucardev.customvalidator.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = LengthValidatorImpl.class)
+public @interface LengthValidator {
+  String message() default "{CustomValidator1.message}";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+  int length();
+}
---
+++ src/main/java/com/gucardev/customvalidator/validator/LengthValidatorImpl.java
@@ -0,0 +1,21 @@
+package com.gucardev.customvalidator.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class LengthValidatorImpl implements ConstraintValidator<LengthValidator, String> {
+  private int length;
+
+  @Override
+  public void initialize(LengthValidator constraintAnnotation) {
+    this.length = constraintAnnotation.length();
+  }
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    if (value == null) {
+      return false;
+    }
+    return value.length() == length;
+  }
+}
